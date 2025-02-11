d132757fab6fc6bbff54e6623e6791a8a8499b41
Baileigh Remy <baileigh.remy@gmail.com>
2025-02-10 20:12:12 -0600

    starter code

---
+++ server/src/services/auth.js
@@ -0,0 +1,33 @@
+const forceDatabaseRefresh = false;
+
+import express from 'express';
+import sequelize from "./config/connection.js";
+import routes from './routes/index.js';
+import Userroutes from './routes/api/userroutes.js';
+// import authMiddleware from './middleware/auth.js'; 
+import path from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.static('../client/dist'));
+app.use(express.json());
+app.use('/api/users', Userroutes);
+// app.use(authMiddleware);
+app.use(routes);
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  });
+
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+});
