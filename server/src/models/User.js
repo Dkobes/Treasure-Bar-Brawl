fc420f4b15d95241c9493d98ff7028ca7713e3a5
Colton Doling <cmdoling@gmail.com>
2025-02-11 22:05:17 -0500

    added User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,35 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            maxLength: 20
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        id: false
+    }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('users', userSchema);
+
 export default User;
