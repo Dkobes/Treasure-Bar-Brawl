df9caa74df0bbd7d6479866bffb5e058f8c80c66
Colton Doling <cmdoling@gmail.com>
2025-02-12 17:48:40 -0500

    added dotenv to server dependencies and added it to connection.js

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/enter database here');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/treasure_bar_brawl');
 
 export default mongoose.connection;
