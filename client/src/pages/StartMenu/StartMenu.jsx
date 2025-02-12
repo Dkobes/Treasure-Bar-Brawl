6c7f0f4949a0c508c6ba277e35cb1bb5aec84c24
Colton Doling <cmdoling@gmail.com>
2025-02-11 20:37:10 -0500

    added basic layout for StartMenu

---
+++ client/src/pages/StartMenu/StartMenu.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import auth from '../../utils/auth';
+
+export const StartMenu = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div>
+            <button>NEW GAME</button>
+            <input
+                name='username'
+                type='text'
+                placeholder='Please enter username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                name='password'
+                type='password'
+                placeholder='Please enter password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button>CONTINUE</button>
+            <input
+                name='username'
+                type='text'
+                placeholder='Please enter username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                name='password'
+                type='password'
+                placeholder='Please enter password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+    )
+}
