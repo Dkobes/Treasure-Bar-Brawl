6b59b079e79a655e6e401205a7879c27c2c4ea2f
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-02-13 19:45:26 -0600

    updates to titlescreen and app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import TitleScreen from './pages/TitleScreen/TitleScreen'
+import StartMenu from './pages/StartMenu/StartMenu'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TitleScreen />} /> 
+        <Route path="/start-menu" element={<StartMenu />} />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/pages/TitleScreen/TitleScreen.jsx
+++ client/src/pages/TitleScreen/TitleScreen.jsx
@@ -1,1 +1,42 @@
 import "nes.css/css/nes.min.css";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const TitleScreen = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="title-screen"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url(/assets/pixelTitle/titlescreen.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+      }}
+    >
+      <button
+        onClick={() => navigate("/start-menu")}
+        style={{
+          marginBottom: "50px",
+          padding: "15px 30px",
+          fontSize: "24px",
+          fontWeight: "bold",
+          backgroundColor: "#ffcc00",
+          border: "none",
+          borderRadius: "10px",
+          cursor: "pointer",
+          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default TitleScreen;
