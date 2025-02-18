dca80d24b874eab2e211d7d12f3b553dbb7f615a
Danyelle <dkobes15@gmail.com>
2025-02-17 20:14:56 -0600

    Fit TitleScreen image with CSS

--- client/src/Game/Game.js
+++ client/src/Game/Game.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import BattleScene from './battleScreen';
+//import BattleScene from './battleScreen';
 
 class Game extends Phaser.Game {
     constructor() {
--- client/src/pages/TitleScreen/TitleScreen.jsx
+++ client/src/pages/TitleScreen/TitleScreen.jsx
@@ -2,41 +2,18 @@ import "nes.css/css/nes.min.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import TitleScreenImage from "../../assets/images/title-screen.jpg";
+import './TitleScreen.css';
 
 const TitleScreen = () => {
   const navigate = useNavigate();
 
   return (
-    <div
-      className="title-screen"
-      style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: `url(${TitleScreenImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-end",
-      }}
-    >
+    <div className="title-screen">
       <button
-        onClick={() => navigate("/start-menu")}
-        style={{
-          marginBottom: "50px",
-          padding: "15px 30px",
-          fontSize: "24px",
-          fontWeight: "bold",
-          backgroundColor: "#ffcc00",
-          border: "none",
-          borderRadius: "10px",
-          cursor: "pointer",
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2)",
-        }}
-      >
+        onClick={() => navigate("/start-menu")}>
         Start
       </button>
-    </div>
+      </div>
   );
 };
 
