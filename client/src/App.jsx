f630cd2f6d1a01b88ddb0aa72354b62e2ebb2c74
Colton Doling <cmdoling@gmail.com>
2025-02-19 20:22:38 -0500

    integrated grid engine into world page and created tilemap display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import TitleScreen from './pages/TitleScreen/TitleScreen'
 import StartMenu from './pages/StartMenu/StartMenu'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import BattleScreen from './pages/BattleScreen/BattleScreen'
 import PartyMenu from './pages/PartyMenu/PartyMenu'
+import WorldScene from './pages/WorldPage/WorldPage'
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
         <Route path="/" element={<TitleScreen />} /> 
         <Route path="/start-menu" element={<StartMenu />} />
         <Route path="/battle" element={<BattleScreen />} />
-        <Route path="party-menu" element={<PartyMenu />} />
+        <Route path="/party-menu" element={<PartyMenu />} />
+        <Route path='/world' element={<WorldScene />} />
       </Routes>
     </Router>
   )
--- client/src/pages/WorldPage/WorldPage.jsx
+++ client/src/pages/WorldPage/WorldPage.jsx
@@ -1,17 +1,28 @@
 import "nes.css/css/nes.min.css";
 import Phaser from 'phaser';
+import { GridEngine } from 'grid-engine';
 import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import './WorldPage.css';
 
-export const WorldScene = () => {
+export const WorldScreen = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
         const config = {
             type: Phaser.AUTO,
+            plugins: {
+                scene: [
+                    {
+                        key: "gridEngine",
+                        plugin: GridEngine,
+                        mapping: "gridEngine",
+                    },
+                ],
+            },
             width: 800,
             height: 600,
+            pixelArt: true,
             parent: 'phaser-game',
             scene: [WorldScene],
         };
@@ -36,16 +47,37 @@ const WorldScene = class extends Phaser.Scene {
 
     preload () {
         // add in correct items to load
-        this.load.image();
-        this.load.image();
+        this.load.image('colton', '../src/assets/playerSprite/colton.png');
+        this.load.image('baseTiles', '../src/assets/images/bar-base-tileset.png');
+        this.load.image('furnitureTiles', '../src/assets/images/bar-furniture-tileset.png');
+        this.load.tilemapTiledJSON('tilemap', '../src/assets/maps/treasure-bar-map.json');
     }
 
     create() {
         //bar
-        this.add.image(400, 300, '') //add in correct image name for bar;
+        this.cameras.main.setZoom(2);
+        const tilemap = this.make.tilemap({ key: "tilemap" });
+        tilemap.addTilesetImage("bar-base-tileset", "baseTiles");
+        tilemap.addTilesetImage("bar-furniture-tileset", "furnitureTiles");
+        tilemap.createLayer(0, "bar-base-tileset", 275, 175);
+        tilemap.createLayer(1, "bar-furniture-tileset", 275, 175);
 
         //player sprite
-        this.player = this.add.sprite(400, 300, 'player').setScale(0.5); //add in correct name for player
+        this.player = this.add.sprite(0, 0, 'colton').setScale(0.75); //add in correct name for player
+
+        //creates tilemap
+        const gridEngineConfig = {
+            characters: [
+                {
+                    id: "colton",
+                    sprite: this.player,
+                    startPosition: { x: 9.5, y: 12.5 },
+                    offsetY: -4,
+                },
+            ],
+        };
+
+        this.gridEngine.create(tilemap, gridEngineConfig);
 
         //arrow key input for movement
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -66,4 +98,4 @@ const WorldScene = class extends Phaser.Scene {
     }
 };
 
-export default WorldScene;
+export default WorldScreen;
