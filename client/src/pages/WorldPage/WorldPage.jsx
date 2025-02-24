c00e8db6b57944c4ea352646a3850bdad9eba9ff
Colton Doling <cmdoling@gmail.com>
2025-02-23 20:16:10 -0500

    added rest of player sprites and updated text to only appear when enemies are dead in world page

--- client/src/pages/WorldPage/WorldPage.jsx
+++ client/src/pages/WorldPage/WorldPage.jsx
@@ -51,7 +51,10 @@ const WorldScene = class extends Phaser.Scene {
         this.load.image('iceElf', '../src/assets/enemySprite/iceElf.png');
         this.load.image('grandma', '../src/assets/enemySprite/grandma.png');
         this.load.image('stan', '../src/assets/enemySprite/sorcererStan.png');
+        this.load.image('baileigh', '../src/assets/playerSprite/baileigh.png');
         this.load.image('colton', '../src/assets/playerSprite/colton.png');
+        this.load.image('dany', '../src/assets/playerSprite/dany.png');
+        this.load.image('tyler', '../src/assets/playerSprite/tyler.png');
         this.load.image('baseTiles', '../src/assets/images/bar-base-tileset.png');
         this.load.image('furnitureTiles', '../src/assets/images/bar-furniture-tileset.png');
         this.load.tilemapTiledJSON('tilemap', '../src/assets/maps/treasure-bar-map.json');
@@ -66,22 +69,32 @@ const WorldScene = class extends Phaser.Scene {
         tilemap.createLayer(0, "base", 0, 0);
         tilemap.createLayer(1, "furniture", 0, 0);
 
-        //player sprite
-        this.player = this.add.sprite(0, 0, 'colton').setScale(0.75); //add in correct name for player
-        this.skeleton = this.add.sprite(0, 0, 'skeleton').setScale(0.75);
-        this.vampirate = this.add.sprite(0, 0, 'vampirate').setScale(0.75);
-        this.elf = this.add.sprite(0, 0, 'iceElf').setScale(0.75);
-        this.grandma = this.add.sprite(0, 0, 'grandma').setScale(0.75);
-        this.stan = this.add.sprite(0, 0, 'stan').setScale(0.75);
-        this.cameras.main.setScroll(-275, -175)
+        //player sprites
+        this.baileigh = this.add.sprite(0, 0, 'baileigh').setScale(0.75);
+        this.baileigh.setVisible(false);
+        this.colton = this.add.sprite(0, 0, 'colton').setScale(0.75);
+        this.colton.setVisible(true);
+        this.dany = this.add.sprite(0, 0, 'dany').setScale(0.75);
+        this.dany.setVisible(false);
+        this.tyler = this.add.sprite(0, 0, 'tyler').setScale(0.75);
+        this.tyler.setVisible(false);
+        this.player = this.colton; //Placeholder
 
+        //enemy sprites
+        this.skeleton = this.add.sprite(0, 0, 'skeleton').setScale(0.75).setState('ALIVE');
+        this.vampirate = this.add.sprite(0, 0, 'vampirate').setScale(0.75).setState('ALIVE');
+        this.elf = this.add.sprite(0, 0, 'iceElf').setScale(0.75).setState('ALIVE');
+        this.grandma = this.add.sprite(0, 0, 'grandma').setScale(0.75).setState('ALIVE');
+        this.stan = this.add.sprite(0, 0, 'stan').setScale(0.75).setState('ALIVE');
+
+        this.cameras.main.setScroll(-275, -175);
         this.txt = this.add.text(80, 257, 'Press space to battle', { font: '"Press Start 2P"', color: '#000000' });
 
         //creates tilemap
         const gridEngineConfig = {
             characters: [
                 {
-                    id: "colton",
+                    id: "player",
                     sprite: this.player,
                     startPosition: { x: 1, y: 7},
                     offsetY: -4,
@@ -125,36 +138,36 @@ const WorldScene = class extends Phaser.Scene {
     update() {
         //arrow key input for movement
         this.cursors = this.input.keyboard?.createCursorKeys();
-        this.gridEngine.setSpeed("colton", 3);
-        const pos = JSON.stringify(this.gridEngine.getPosition("colton"));
+        this.gridEngine.setSpeed("player", 3);
+        const pos = JSON.stringify(this.gridEngine.getPosition("player"));
         
         if (this.cursors.left.isDown) {
-            this.gridEngine.move("colton", "left");
+            this.gridEngine.move("player", "left");
         } else if (this.cursors.right.isDown) {
-            this.gridEngine.move("colton", "right");
+            this.gridEngine.move("player", "right");
         } else if (this.cursors.up.isDown) {
-            this.gridEngine.move("colton", "up");
+            this.gridEngine.move("player", "up");
         } else if (this.cursors.down.isDown) {
-            this.gridEngine.move("colton", "down");
+            this.gridEngine.move("player", "down");
         }
 
-        if (pos === '{"x":1,"y":3}') {
+        if (pos === '{"x":1,"y":3}' && this.skeleton.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":3,"y":3}') {
+        } else if (pos === '{"x":3,"y":3}' && this.vampirate.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":4,"y":2}') {
+        } else if (pos === '{"x":4,"y":2}' && this.vampirate.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":6,"y":3}') {
+        } else if (pos === '{"x":6,"y":3}' && this.elf.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":7,"y":4}') {
+        } else if (pos === '{"x":7,"y":4}' && this.elf.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":7,"y":6}') {
+        } else if (pos === '{"x":7,"y":6}' && this.grandma.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":6,"y":7}') {
+        } else if (pos === '{"x":6,"y":7}' && this.grandma.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":5,"y":6}') {
+        } else if (pos === '{"x":5,"y":6}' && this.grandma.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
-        } else if (pos === '{"x":4,"y":0}') {
+        } else if (pos === '{"x":4,"y":0}' && this.stan.state === 'ALIVE') {
             this.txt.setColor('#ffffff');
         } else {
             this.txt.setColor('#000000');
